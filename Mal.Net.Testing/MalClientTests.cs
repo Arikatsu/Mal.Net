f33b587ea9d93805bc880a20e145ca0106bc516d
Scald <104459145+Arikatsu@users.noreply.github.com>
2024-08-28 21:13:43 +0530

    Common test client and `AnimeDetailsTest`

--- Mal.Net.Testing/MalClientTests.cs
+++ Mal.Net.Testing/MalClientTests.cs
@@ -6,8 +6,8 @@ namespace Mal.Net.Testing;
 
 public class MalClientTests
 {
-    private readonly IConfiguration _configuration;
     private readonly ITestOutputHelper _output;
+    private readonly MalClient _client;
 
     public MalClientTests(ITestOutputHelper output)
     {
@@ -17,30 +17,35 @@ public class MalClientTests
             .AddUserSecrets<MalClientTests>()
             .Build();
 
-        _configuration = builder;
+        IConfiguration configuration = builder;
+        
         _output = output;
+        _client = new MalClient(configuration["Secrets:ClientId"]!, configuration["Secrets:ClientSecret"]!);
     }
 
     [Fact]
-    public void AnimeListTest()
+    public async Task AnimeListTest()
     {
-        var clientId = _configuration["Secrets:ClientId"];
-        var clientSecret = _configuration["Secrets:ClientSecret"];
-
-        Assert.NotNull(clientId);
-        Assert.NotNull(clientSecret);
-
-        var client = new MalClient(clientId, clientSecret);
-        
         IEnumerable<string> fields = new List<string> { AnimeFields.Rating, AnimeFields.StartDate };
-        
-        var animeList = client.GetAnimeListAsync("oregairu", 1, 1, fields).Result;
+
+        var animeList = await _client.GetAnimeListAsync("oregairu", 1, 1, fields);
 
         var json = JsonSerializer.Serialize(animeList, new JsonSerializerOptions { WriteIndented = true });
         _output.WriteLine(json);
 
-        client.Dispose();
-
         Assert.NotNull(animeList);
+    }
+    
+    [Fact]
+    public async Task AnimeDetailsTest()
+    {
+        IEnumerable<string> fields = new List<string> { AnimeFields.Rating, AnimeFields.StartDate };
+
+        var animeDetails = await _client.GetAnimeDetailsAsync(33161, fields);
+
+        var json = JsonSerializer.Serialize(animeDetails, new JsonSerializerOptions { WriteIndented = true });
+        _output.WriteLine(json);
+
+        Assert.NotNull(animeDetails);
     }
 }
