f46dc85d522633d23947ebb8083b3161b1173803
Scald <104459145+Arikatsu@users.noreply.github.com>
2024-09-15 18:46:30 +0530

    Custom `JsonConverter` for `Paginated` objects

--- Mal.Net.Library/Schemas/Paginated.cs
+++ Mal.Net.Library/Schemas/Paginated.cs
@@ -1,23 +1,26 @@
 ﻿using System.Text.Json;
 using System.Text.Json.Serialization;
+using Mal.Net.Utils;
 
 namespace Mal.Net.Schemas;
 
 public class Paginated<T>
 {
+    private static readonly JsonSerializerOptions Options = new() { Converters = { new SingleDataConverter<T>() } };
+    
     # region Json Properties
     
     [JsonPropertyName("data")]
     public IEnumerable<T> Data { get; set; } = Enumerable.Empty<T>();
-    
+
     [JsonPropertyName("paging")]
-    public Paging Paging { get; set; } = new Paging();
+    public Paging Paging { get; set; } = new();
     
     #endregion
     
     public static Paginated<T> FromJson(string json)
     {
-        return JsonSerializer.Deserialize<Paginated<T>>(json) ?? throw new JsonException("Failed to deserialize JSON response.");
+        return JsonSerializer.Deserialize<Paginated<T>>(json, Options) ?? throw new JsonException("Failed to deserialize JSON response.");
     }
 }
 
---
+++ Mal.Net.Library/Utils/SingleDataConverter.cs
@@ -0,0 +1,31 @@
+﻿using System.Text.Json;
+using System.Text.Json.Serialization;
+
+namespace Mal.Net.Utils;
+
+public class SingleDataConverter<T> : JsonConverter<IEnumerable<T>>
+{
+    public override IEnumerable<T> Read(ref Utf8JsonReader reader, Type typeToConvert, JsonSerializerOptions options)
+    {
+        if (reader.TokenType == JsonTokenType.StartArray)
+        {
+            var list = new List<T>();
+
+            while (reader.Read() && reader.TokenType != JsonTokenType.EndArray)
+            {
+                var item = JsonSerializer.Deserialize<T>(ref reader, options);
+                list.Add(item!);
+            }
+
+            return list;
+        }
+        
+        var singleItem = JsonSerializer.Deserialize<T>(ref reader, options);
+        return new List<T> { singleItem! };
+    }
+
+    public override void Write(Utf8JsonWriter writer, IEnumerable<T> value, JsonSerializerOptions options)
+    {
+        JsonSerializer.Serialize(writer, value.FirstOrDefault(), options);
+    }
+}
