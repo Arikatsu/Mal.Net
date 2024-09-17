d2d981ca2d1834f1a425af631066c503a011d922
Scald <104459145+Arikatsu@users.noreply.github.com>
2024-09-18 01:04:32 +0530

    User authorization tests

---
+++ Mal.Net.Testing/MainAuthTests/AuthTests.cs
@@ -0,0 +1,69 @@
+﻿using System.Net;
+using System.Diagnostics;
+using Xunit.Abstractions;
+
+namespace Mal.Net.Testing.MainAuthTests;
+
+public class AuthTests : TestsBase
+{
+    public AuthTests(ITestOutputHelper output) : base(output) { }
+    
+    [Fact]
+    public async Task AuthFlowTest()
+    {
+        var authUrl = Client.GenerateAuthUrl(out var state, out var codeVerfier);
+        
+        Assert.NotNull(authUrl);
+        Assert.NotNull(state);
+        Assert.NotNull(codeVerfier);
+        
+        var tcs = new TaskCompletionSource<string>();
+        
+        var server = new HttpListener();
+        server.Prefixes.Add(Config.RedirectUri);
+        server.Start();
+        
+        Process.Start(new ProcessStartInfo(authUrl) { UseShellExecute = true });
+
+        server.BeginGetContext(result =>
+        {
+            var context = server.EndGetContext(result);
+            var request = context.Request;
+            var code = request.QueryString["code"] ?? "error";
+            
+            tcs.SetResult(code);
+
+        }, null);
+        
+        var code = await tcs.Task;
+        
+        server.Stop();
+        
+        Assert.NotEqual("error", code);
+        
+        var user = await Client.AuthenticateUser(code, codeVerfier);
+        
+        Assert.NotNull(user.AccessToken);
+        Assert.NotNull(user.RefreshToken);
+        
+        ConfigManager.Update(config =>
+        {
+            config.AccessToken = user.AccessToken;
+            config.RefreshToken = user.RefreshToken;
+        });
+    }
+    
+    [Fact]
+    public async Task RefreshTokenTest()
+    {
+        var previousAccessToken = User.AccessToken;
+        var previousRefreshToken = User.RefreshToken;
+        
+        await User.RefreshAccessToken(true);
+        
+        Assert.NotEqual(previousAccessToken, User.AccessToken);
+        Assert.NotEqual(previousRefreshToken, User.RefreshToken);
+    }
+}
+
+
